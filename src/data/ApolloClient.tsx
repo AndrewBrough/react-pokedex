173243451fa85aa567f7c6f06db3e559bcdf9cc0
Andrew Brough <andrew.brough@fullscript.com>
2022-08-01 19:15:42 -0400

    added persisted storage between sessions

--- src/data/ApolloClient.tsx
+++ src/data/ApolloClient.tsx
@@ -1,8 +1,19 @@
 import { FC } from "react";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { LocalStorageWrapper, persistCache } from "apollo3-cache-persist";
 
 const ApolloClientProvider: FC = ({ children }) => {
   const cache = new InMemoryCache();
+
+  const getPersistedStorage = async () => {
+    await persistCache({
+      cache,
+      storage: new LocalStorageWrapper(window.localStorage),
+    });
+  };
+
+  getPersistedStorage();
+
   const client = new ApolloClient({
     uri: "https://beta.pokeapi.co/graphql/v1beta",
     cache,
